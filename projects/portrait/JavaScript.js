9fc5e98802cbd33a55141e030f27ceb060ca2a21
NadiaNayr <aidanconorryan@outlook.com>
2026-02-13 22:42:18 -0500

    made circle

--- projects/portrait/JavaScript.js
+++ projects/portrait/JavaScript.js
@@ -10,3 +10,7 @@ ctx.fillRect(100, 100, 200, 300);
 //draw a red sqaure
 ctx.fillStyle = "red";
 ctx.fillRect(350, 100, 50, 50);
+
+//draw a circle
+const ctx = document.getElementById('canvas').getContext('2d');
+ctx.arc(50, 50, 40, 0, 7); ctx.fill(); 
