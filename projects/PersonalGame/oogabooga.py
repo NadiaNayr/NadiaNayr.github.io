93a073141ff6119d90d6e03635c9f9e985f372c1
NadiaNayr <aidanconorryan@outlook.com>
2026-03-04 14:47:00 -0500

    bob

--- projects/PersonalGame/oogabooga.py
+++ projects/PersonalGame/oogabooga.py
@@ -1,6 +1,24 @@
 import random
 import pygame
 
+# pygame setup
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Ooga Booga")
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("purple")
+ 
+    # GAME HERE
+
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
