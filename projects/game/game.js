00fa1fc224848922ff79d39a7604bc0ad96c0fd9
NadiaNayr <aidanconorryan@outlook.com>
2026-03-09 21:47:26 -0400

    changed base code names. added variable/functions

--- projects/game/game.js
+++ projects/game/game.js
@@ -2,20 +2,24 @@ let gameActive = true; //this variable is required.
                        //to stop the game, set it to false.
 
 //Declare your other global variables here
-
+let Time = 0
+let EnergyLevel = 0
 
 //If you need, add any "helper" functions here
-
+function AddTime() {
+    //make it wait
+    Time = Time + 1
+}
 
 //Make one function for each location
 function locationA() {
     clear();
-    print("\nYou are in location A!");
+    print("\nYou are in Prithvi's bedroom");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationB");
+        "\n\t Bathroom");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationb") {
+        if (input.toLowerCase() === "Bathroom") {
             locationB();
         } else {
             stayHere();
@@ -27,9 +31,9 @@ function locationA() {
 
 function locationB() {
     clear();
-    print("\nYou are in location B!");
+    print("\nYou are in the bathroom!");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationA");
+        "\n\t Prithvi's bedroom");
     
     function processInput(input){
         if (input.toLowerCase() === "locationa") {
