a6b96d9bc43c700e0f6b3717e9de270cbd158dd7
NadiaNayr <aidanconorryan@outlook.com>
2026-03-17 08:46:33 -0400

    bob

--- practice/bounce.js
+++ practice/bounce.js
@@ -2,19 +2,19 @@ const canvas = document.getElementById("MyCanvas")
 const ctx = canvas.getContext("2d")
 
 let x = 0;
-let y = 2;
+let y = 6;
 let vx = 0;
-let vy = 2;
+let vy = 7;
 
 function move(){
     ctx.clearRect(x, 0, 800, 800);
     x = x + vx;
     y = y + vy;
 
-    if (x > 800 || x < 0){
+    if (x > 750 || x < 0){
         vx = vx * -1;
     }
-    if (y > 800 || y < 0){
+    if (y > 750 || y < 0){
         vy = vy * -1;
     }
     ctx.fillRect(x, y, 50, 50);
