e12324c14d39fbacf0344418596fc71897e634c2
NadiaNayr <aidanconorryan@outlook.com>
2026-03-17 09:42:20 -0400

    bob

---
+++ practice/keys.js
@@ -0,0 +1,21 @@
+const canvas = document.getElementById("MyCanvas")
+const ctx = canvas.getContext("2d")
+
+let x = 3;
+let y = 2;
+let vx = 3;
+let vy = 4;
+let gravity = 1;
+
+function move(){
+    ctx.clearRect(x, 0, 800, 800);
+    if (x > 750 || x < 0){
+        vx = vx * -1;
+    }
+    if (y > 750 || y < 0){
+        vy = vy * -1;
+    }
+    ctx.fillRect(x, y, 50, 50);
+    requestAnimationFrame(move);
+}
+move();
